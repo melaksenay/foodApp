06e8b461e7af28ddd7766f259248ac2aaf7efb0e
Logan Farrow <farrowlog@gmail.com>
2023-11-08 13:50:28 -0600

    basic storyboard added, app created

---
+++ FoodNut/FoodNut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodNut
+//
+//  Created by Logan Farrow on 11/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
