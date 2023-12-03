1c4cef445bd87d03850b196e27f7d28bc603fd0c
Logan Farrow <farrowlog@gmail.com>
2023-12-02 18:14:15 -0600

    test

--- FoodNut/FoodNut/Home.swift
+++ FoodNut/FoodNut/Home.swift
@@ -33,6 +33,8 @@ class Home: UIViewController, UICollectionViewDataSource, UICollectionViewDelega
                 }
             }
             recentsCollectionView.reloadData()
+            
+            print(recentProducts)
         }
 
     
